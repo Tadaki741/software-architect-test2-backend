4475435d9cfb1dda25badbe07eb6d0c572a69e22
Tadaki741 <94947002+Tadaki741@users.noreply.github.com>
2023-01-11 11:02:32 +0700

    Initial commit

---
+++ src/main/java/com/example/softwarearchitecttest2backend/SoftwareArchitectTest2BackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.softwarearchitecttest2backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareArchitectTest2BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwareArchitectTest2BackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/softwarearchitecttest2backend/SoftwareArchitectTest2BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.softwarearchitecttest2backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwareArchitectTest2BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
