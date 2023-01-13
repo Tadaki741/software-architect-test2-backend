f8a5bf54f73fe8b285f7e7c2787e3dcf26e5dbbf
Tadaki741 <94947002+Tadaki741@users.noreply.github.com>
2023-01-14 01:32:42 +0700

    Turn on Kafka and Redis

--- src/main/java/com/example/softwarearchitecttest2backend/kafka/Consumer.java
+++ src/main/java/com/example/softwarearchitecttest2backend/kafka/Consumer.java
@@ -1,44 +1,40 @@
-//package com.example.softwarearchitecttest2backend.kafka;
-//
-//import com.example.softwarearchitecttest2backend.BillOfMaterial.BOMService;
-//import com.example.softwarearchitecttest2backend.ManufacturingOrder.MOService;
-//import com.example.softwarearchitecttest2backend.Product.Product;
-//import com.example.softwarearchitecttest2backend.Product.ProductService;
-//import com.example.softwarearchitecttest2backend.ProductOrder.POService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.kafka.annotation.KafkaListener;
-//import org.springframework.stereotype.Service;
-//
-//import java.io.IOException;
-//import java.util.logging.Logger;
-//
-//@Service
-//public class Consumer {
-//
-//    private final Logger logger = Logger.getLogger(Consumer.class.getName());
-//
-//    @Autowired
-//    private ProductService productService;
-//
-//    @Autowired
-//    private POService poService;
-//
-//
-//    @Autowired
-//    private BOMService bomService;
-//
-//
-//    @Autowired
-//    private MOService moService;
-//
-//    @KafkaListener(topics = "product", groupId = "group_id")
-//    public void consume(Product product) throws IOException {
-//        logger.info(String.format("#### -> Consumed message -> %s", product));
-//
-//        //save message to database
-//        Product newProduct = this.productService.addNewProduct(product);
-//
-//        logger.info(String.format("#### -> ID message -> %s", newProduct.getId()));
-//
-//    }
-//}
+package com.example.softwarearchitecttest2backend.kafka;
+
+import com.example.softwarearchitecttest2backend.BillOfMaterial.BOMService;
+import com.example.softwarearchitecttest2backend.ManufacturingOrder.MOService;
+import com.example.softwarearchitecttest2backend.Product.Product;
+import com.example.softwarearchitecttest2backend.Product.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+
+@Service
+public class Consumer {
+
+    private final Logger logger = Logger.getLogger(Consumer.class.getName());
+
+    @Autowired
+    private ProductService productService;
+
+
+    @Autowired
+    private BOMService bomService;
+
+
+    @Autowired
+    private MOService moService;
+
+    @KafkaListener(topics = "product", groupId = "group_id")
+    public void consume(Product product) throws IOException {
+        logger.info(String.format("#### -> Consumed message -> %s", product));
+
+        //save message to database
+        Product newProduct = this.productService.addNewProduct(product);
+
+        logger.info(String.format("#### -> ID message -> %s", newProduct.getId()));
+
+    }
+}
--- src/main/java/com/example/softwarearchitecttest2backend/kafka/Producer.java
+++ src/main/java/com/example/softwarearchitecttest2backend/kafka/Producer.java
@@ -1,22 +1,22 @@
-//package com.example.softwarearchitecttest2backend.kafka;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.kafka.core.KafkaTemplate;
-//import org.springframework.stereotype.Service;
-//
-//import java.util.logging.Logger;
-//
-//@Service
-//public class Producer {
-//
-//    private static final Logger logger = Logger.getLogger(Producer.class.getName());
-//    private static final String TOPIC = "users";
-//
-//    @Autowired
-//    private KafkaTemplate<String, String> kafkaTemplate;
-//
-//    public void sendMessage(String message) {
-//        logger.info(String.format("#### -> Producing message -> %s", message));
-//        this.kafkaTemplate.send(TOPIC, message);
-//    }
-//}
+package com.example.softwarearchitecttest2backend.kafka;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.logging.Logger;
+
+@Service
+public class Producer {
+
+    private static final Logger logger = Logger.getLogger(Producer.class.getName());
+    private static final String TOPIC = "users";
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    public void sendMessage(String message) {
+        logger.info(String.format("#### -> Producing message -> %s", message));
+        this.kafkaTemplate.send(TOPIC, message);
+    }
+}
